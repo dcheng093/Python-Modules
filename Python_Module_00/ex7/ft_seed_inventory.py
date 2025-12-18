333157e3d7a618861f29f7c0f1ef1e899064f47f
Daniel Cheng <dcheng@u81z08s04.42kl.edu.my>
2025-12-18 20:06:48 +0800

    stuff

--- Python_Module_00/ex7/ft_seed_inventory.py
+++ Python_Module_00/ex7/ft_seed_inventory.py
@@ -1,9 +1,9 @@
 def ft_seed_inventory(seed: str, count: int, unit: str) -> None:
-    if unit not in ("grams", "packets", "area"):
-        unit = "Unknown unit type"
     if unit == "packets":
         print(seed.capitalize(), "seeds:", count, unit, "available")
-    if unit == "grams":
+    elif unit == "grams":
         print(seed.capitalize(), "seeds:", count, unit, "total")
-    if unit == "area":
+    elif unit == "area":
         print(seed.capitalize(), "seeds: covers", count, "square meters")
+    else:
+        unit = "Unknown unit type"
